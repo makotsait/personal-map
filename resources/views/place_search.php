b3661b3e09528f075afc6eeaff618e5a7798a249
M.Saito <makotsait@gmail.com>
2019-12-03 03:53:46 +0900

    Add place detail search by place_id func

--- resources/views/place_search.php
+++ resources/views/place_search.php
@@ -1,8 +1,10 @@
 <?php
 function searchPlace($keyword) {
 	$api_key = "AIzaSyA-OXjQyOAsZIuDqm6FDUDqp3vNRLMNhE8";
+	$place_id="ChIJ__9bxx2MGGARWfSFpLzW3pU";
 	$keyword = urlencode($keyword);
-	$url = "https://maps.googleapis.com/maps/api/place/textsearch/json?key={$api_key}&query={$keyword}";
+	// $url = "https://maps.googleapis.com/maps/api/place/textsearch/json?key={$api_key}&query={$keyword}";
+	$url = "https://maps.googleapis.com/maps/api/place/details/json?key={$api_key}&place_id={$place_id}&language=ja";
 	$json = file_get_contents($url);
 	return json_decode($json, true);
 }
