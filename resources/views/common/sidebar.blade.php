32ec1b1e403aee9b84ce185c61fbdbe35a9ae7c6
M.Saito <makotsait@gmail.com>
2020-01-28 19:40:33 +0900

    Add: sidebar on mainpage

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -1,3 +1,31 @@
+
 @section('sidebar')
-<p style="padding: 20px;">test</p>
+
+<div class="sidebar left" >Hello World</div>
+
+<script>
+$(document).ready(function () {
+    // 向き
+    var sides = ["left", "top", "right", "bottom"];
+
+    // サイドバーの初期化
+    for (var i = 0; i < sides.length; ++i) {
+        var cSide = sides[i];
+        $(".sidebar." + cSide).sidebar({side: cSide});
+    }
+
+    // ボタンのクリックにより...
+    $(".btn[data-action]").on("click", function () {
+        var $this = $(this);
+        var action = $this.attr("data-action");
+        var side = $this.attr("data-side");
+        $(".sidebar." + side).trigger("sidebar:" + action);
+        return false;
+    });
+});
+</script>
+
+<a href="#" class="btn" data-action="toggle" data-side="left" style="margin-left: 300px;">
+  Toggle Left
+</a>
 @show
