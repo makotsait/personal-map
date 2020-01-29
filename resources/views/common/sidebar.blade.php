cac67039c771e3ba038f20e744628c6245498043
M.Saito <makotsait@gmail.com>
2020-01-29 23:55:41 +0900

    Add: display facility photo on sidebar

--- resources/views/common/sidebar.blade.php
+++ resources/views/common/sidebar.blade.php
@@ -2,7 +2,13 @@
 @section('sidebar')
 
 <div class="sidebar left" >Hello World
-<img class="sidebar close btn" data-action="toggle" data-side="left" border="0" src="{{ asset('icon/icon_open_sidebar.png') }}" alt="イラスト1">
+<img class="sidebar close btn" data-action="toggle" data-side="left" border="0" src="{{ asset('icon/icon_open_sidebar.png') }}" alt="icon_sidebar_oc">
+<?php
+$top_img_url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&key=AIzaSyA-OXjQyOAsZIuDqm6FDUDqp3vNRLMNhE8";
+$photo_ref = "photoreference=CmRaAAAAiVgVE_r8zAmHGceo65OoPPa4tUqawrI0OiuHrxH_wKVyJ2NCEv_bnvFUeuwbPX8liS2XGC_sfuBUJdh48leGihuC8UixzwBHtjPtxuhQnE1OTekd78nUr-eqQWPII3KoEhCv9Ixq_3zcDqMtEGFJLSN4GhSExSvn9LvQFJga4bqAmtqCYn7gAg";
+$top_img_url =  $top_img_url."&".$photo_ref;
+echo "<img src=$top_img_url>";
+?>
 </div>
 
 <script>
@@ -26,4 +32,5 @@ $(document).ready(function () {
     });
 });
 </script>
+
 @show
