5b5b9d7cbd682dd3c0c85abb002b3c06645a0b53
M.Saito <makotsait@gmail.com>
2020-02-03 17:21:28 +0900

    Fix: issue of unable to get text from TestController.php in index.blade.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,6 +8,14 @@ class TestController extends Controller
 {
     public function test()
     {
-        echo $_POST['name1'];
+        $array = $this->searchPlace($_POST['name1']);
+        echo($array["result"]["formatted_address"]);
+    }
+
+    public function searchPlace($place_id) {
+        $api_key = "AIzaSyA-OXjQyOAsZIuDqm6FDUDqp3vNRLMNhE8";
+        $url = "https://maps.googleapis.com/maps/api/place/details/json?key={$api_key}&place_id={$place_id}&language=ja";
+        $json = file_get_contents($url);
+        return json_decode($json, true);
     }
 }
