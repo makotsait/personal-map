497fc835a95b55d69f9287db204005ec1f9bcd41
M.Saito <makotsait@gmail.com>
2020-02-03 18:26:42 +0900

    Fix: rename TestController.php to PlaceApiController.php

--- app/Http/Controllers/PlaceApiController.php
+++ app/Http/Controllers/PlaceApiController.php
@@ -4,18 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TestController extends Controller
+class PlaceApiController extends Controller
 {
-    public function test()
+    public function getPlaceDetail()
     {
-        $array = $this->searchPlace($_POST['name1']);
-        echo($array["result"]["formatted_address"]);
-    }
-
-    public function searchPlace($place_id) {
+        $place_id = $_POST['place_id'];
         $api_key = "AIzaSyA-OXjQyOAsZIuDqm6FDUDqp3vNRLMNhE8";
         $url = "https://maps.googleapis.com/maps/api/place/details/json?key={$api_key}&place_id={$place_id}&language=ja";
         $json = file_get_contents($url);
-        return json_decode($json, true);
+        return $json;
     }
 }
+
