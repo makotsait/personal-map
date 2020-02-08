a41f68bb77211c61b06873a24a4439afae1a7d2e
M.Saito <makotsait@gmail.com>
2020-02-08 23:45:54 +0900

    Add: get header-image from api

--- app/Http/Controllers/place_search.php
+++ app/Http/Controllers/place_search.php
@@ -9,10 +9,17 @@ function searchPlace($place_id) {
 	return json_decode($json, true);
 }
 // print_r(searchPlace("東京駅"));
-
-$array = searchPlace("ChIJH7qx1tCMGGAR1f2s7PGhMhw");
+// place detailのjson取得
+// $url = "https://maps.googleapis.com/maps/api/place/details/json?key=AIzaSyA-OXjQyOAsZIuDqm6FDUDqp3vNRLMNhE8&place_id=ChIJH7qx1tCMGGAR1f2s7PGhMhw&language=ja";
+// place head imgの取得
+$url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=500&key=AIzaSyA-OXjQyOAsZIuDqm6FDUDqp3vNRLMNhE8&photoreference=CmRaAAAArHCqh8wKJg94Bv_ksvzAhz1IW03ygjkY8UPOKdSwsRRCa_Gx4yLXVqMxuo7XyfNL4djtVFOL8Q-nze21zxQGvNlPfbct8Wm8x-k1XHBBnsw1QhTgBehNpYcQ26DjWEqHEhBPNkoyy9_wRyKnAM9iZaiOGhSUkCtenJ81LJXNoEcsrU-ZB8e98A";
+$header=get_headers($url);
+print_r($header);
+$img_url = preg_replace('@^Location: @','',$header[5]);
+echo $img_url;
+// $array = searchPlace("ChIJH7qx1tCMGGAR1f2s7PGhMhw");
 // var_dump($array);
-echo($array["result"]["formatted_address"]);
+// echo($array["result"]["photos"][0]["photo_reference"]);
 // $opening_hours = $array["result"]["opening_hours"]["weekday_text"];
 // // var_dump($opening_hours);
 // foreach($opening_hours as $oh){
