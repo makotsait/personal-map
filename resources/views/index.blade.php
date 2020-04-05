4714e992bbf204f0cf49cc6d4025e81ea86f7500
M.Saito <makotsait@gmail.com>
2020-04-05 13:21:40 +0900

    Create: map-control.js, Move: map related functions from index to map-control

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,6 +12,7 @@
   <script src="{{ asset('/js/jquery.sidebar.min.js') }}"></script>
   <script src="{{ asset('/js/perfect-scrollbar.min.js') }}"></script>
   <script src="{{ asset('/js/markerclusterer.js') }}"></script>
+  <script src="{{ asset('/js/map-control.js') }}"></script>
   <!-- <script src="https://unpkg.com/@google/markerclustererplus@4.0.1/dist/markerclustererplus.min.js"></script> -->
 </head>
 
@@ -28,19 +29,6 @@
   </div>
 
   <script>
-    var ready = { api: false, ajax: false };
-    var map;
-    var mc;
-    var mapData;
-    var mapOptions = {
-    center: { // 地図の緯度経度
-        lat: 35.685614,
-        lng: 139.752878
-    },
-        zoom: 14, // 地図の拡大率
-        mapTypeControl: false, // マップ切り替えのコントロールを表示するかどうか
-        streetViewControl: false // ストリートビューのコントロールを表示するかどうか
-    }
     var place_locations = [
         {
             "name": "AKIBAカルチャーズZONE",
@@ -122,9 +110,7 @@
 
     function initMap() {
       map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
       var input = document.getElementById('pac-input');
-
       var autocomplete = new google.maps.places.Autocomplete(input);
       autocomplete.bindTo('bounds', map);
 
@@ -157,9 +143,6 @@
         return map;
       }
 
-      
-
-
       function setPlaceDetail(place) {
         infowindowContent.children['place-name'].textContent = place.name;
         infowindowContent.children['place-id'].textContent = place.place_id;
@@ -172,11 +155,9 @@
         infowindow.close();
 
         var place = autocomplete.getPlace();
-
         if (!place.geometry) {
           return;
         }
-
         map = setBounds(map, place);
 
         // Set the position of the marker using the place ID and location.
@@ -184,86 +165,14 @@
           placeId: place.place_id,
           location: place.geometry.location
         });
+        marker.setVisible(true);
 
         getPlaceDetail(place.place_id);
         localStorage.clear('ratings_json');
 
         getRatings(place.place_id, null);
-
-        marker.setVisible(true);
-
         setPlaceDetail(place);
-        infowindow.open(map, marker);
-      });
-    }
-
-    /**
-    * Google Maps APIの準備完了後の処理
-    */
-    function api_ready() {
-        ready['api'] = true;
-        generate_map();
-    }
-    
-    /**
-    * 地図を生成する
-    */
-    function generate_map() {
-        if(ready['api'] && ready['ajax']) {
-            map = new google.maps.Map(document.getElementById('map'), mapOptions);
-            add_marker();
-        }
-    }
-
-    function add_marker() {
-      var markers = [];
-      for (var i = 0; i < place_locations.length; i++) {
-        var item = place_locations[i];
-
-        // マーカーの設置
-        var marker = new google.maps.Marker({
-            position: item['latlng']
-        });
-
-        // 吹き出しの生成
-        var ins = '<div class="map-window">';
-        ins += '<p class="map-window_name">' + item['name'] + '</p>';
-        ins += '</div>';
-        var infoWindow = new google.maps.InfoWindow({
-          content: ins
-        });
-
-        // マーカーのイベント設定
-        add_event_to_marker(marker, infoWindow, i);
-
-        // MarkerClusterer用にマーカーの情報を配列にまとめる
-        markers.push(marker);
-      }
-      mc = new MarkerClusterer(map, markers);
-    }
-
-    /**
-      * マーカーにイベントを追加する
-      * @param {object} marker     (required) マーカーの情報
-      * @param {object} infoWindow (required) 吹き出しの情報
-      * @param {number} index      (required) 地図情報のインデックス番号
-      */
-
-    function infoWindows_hide() {
-      for (var i = 0; i < place_locations.length; i++) {
-        place_locations[i]['infoWindow'].close();
-      }
-    }
-    
-    function add_event_to_marker(marker, infoWindow, index) {
-      var item = place_locations[index];
-      item['marker'] = marker;
-      item['infoWindow'] = infoWindow;
-  
-      // マーカークリック時に吹き出しを表示する
-      item['marker'].addListener('click', function(e) {
-          infoWindows_hide();
-          item['infoWindow'].open(map, item['marker']);
+        // infowindow.open(map, marker);
       });
     }
 
