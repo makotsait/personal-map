c96188b8ca7a7a0f148aa83740d16d686bbfdb32
M.Saito <makotsait@gmail.com>
2020-04-18 16:38:15 +0900

    Refactor: function createPlace

--- app/Models/Place.php
+++ app/Models/Place.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Place extends Model
 {
     protected $primaryKey = 'place_id';
-    protected $guarded = 'place_id';
+    protected $guarded = ['place_id'];
 }
