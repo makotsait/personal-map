d79e144ed2df975b076a74f22116f5e56976d289
M.Saito <makotsait@gmail.com>
2020-04-18 21:08:44 +0900

    Delete: create infowindow

--- public/js/map-control.js
+++ public/js/map-control.js
@@ -15,71 +15,42 @@ center: { // 地図の緯度経度
 /**
 * 地図を生成する
 */
-function generate_map() {
-    if(ready['map'] && ready['locations']) {
-        // map = new google.maps.Map(document.getElementById('map'), mapOptions);
-        add_marker();
-    }
-}
-
 function add_marker() {
-  var markers = [];
-  for (var i = 0; i < place_locations.length; i++) {
-    var item = place_locations[i];
+    if(ready['map'] && ready['locations']) {
+        var markers = [];
+        for (var i = 0; i < place_locations.length; i++) {
+        var item = place_locations[i];
 
-    // マーカーの設置
-    var marker = new google.maps.Marker({
-        position: item['latlng']
-    });
+        // マーカーの設置
+        var marker = new google.maps.Marker({
+            position: item['latlng']
+        });
 
-    // 吹き出しの生成
-    var ins = '<div class="map-window">';
-    // ins += '<p class="map-window_name">' + item['name'] + '</p>';
-    ins += '</div>';
-    var infoWindow = new google.maps.InfoWindow({
-      content: ins
-    });
+        // マーカーのイベント設定
+        add_event_to_marker(marker, i);
 
-    // マーカーのイベント設定
-    add_event_to_marker(marker, infoWindow, i);
-
-    // MarkerClusterer用にマーカーの情報を配列にまとめる
-    markers.push(marker);
-  }
-  mc = new MarkerClusterer(map, markers);
+        // MarkerClusterer用にマーカーの情報を配列にまとめる
+        markers.push(marker);
+        }
+        mc = new MarkerClusterer(map, markers);
+    }
 }
 
 /**
   * マーカーにイベントを追加する
   * @param {object} marker     (required) マーカーの情報
-  * @param {object} infoWindow (required) 吹き出しの情報
   * @param {number} index      (required) 地図情報のインデックス番号
   */
 
-function infoWindows_hide() {
-  for (var i = 0; i < place_locations.length; i++) {
-    place_locations[i]['infoWindow'].close();
-  }
-}
-
-function add_event_to_marker(marker, infoWindow, index) {
+function add_event_to_marker(marker, index) {
     let item = place_locations[index];
     item['marker'] = marker;
-    item['infoWindow'] = infoWindow;
-
     item['marker'].addListener('click', function() {
         google_place_id = item['google_place_id'];
-
-        //選択されたoption番号を取得
-        // var index = this.selectedIndex;
-        // document.getElementById('form_place_type_id').value = options[index].value;
-        // place_type_id = options[inplace_type_iddex].value;
 
         fetchPlaceDetails(google_place_id);
         localStorage.clear('ratings_json');
 
         getRatings(google_place_id, null);
-        //   infoWindows_hide();
-        //   item['infoWindow'].open(map, item['marker']);
   });
 }
